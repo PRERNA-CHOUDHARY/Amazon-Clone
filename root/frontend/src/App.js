9a9db85102c2ed48bf96c5ef18b37a3300e52a03
Prerna Choudhary <choudharyprerna493@gmail.com>
2023-08-15 17:31:22 +0530

    files added

---
+++ root/frontend/src/App.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    // BEM
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/checkout" element={[<Header />, <h1>I am a checkout page</h1>]}/>
+          <Route path="/" element={[<Header />, <Home />]}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
